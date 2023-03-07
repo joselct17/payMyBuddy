746b093cb81c5bc6cfaf0c28cc2d9056e75c97a5
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-07 12:03:35 +0100

    creation du test getBankTransaction

--- src/test/java/com/joselct17/PayMyBuddy/controller/BankTransactionControllerTest.java
+++ src/test/java/com/joselct17/PayMyBuddy/controller/BankTransactionControllerTest.java
@@ -1,5 +1,30 @@
 package com.joselct17.PayMyBuddy.controller;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BankTransactionControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void getBankTransaction() throws Exception {
+        mockMvc.perform(get("/banktransaction"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
 }
