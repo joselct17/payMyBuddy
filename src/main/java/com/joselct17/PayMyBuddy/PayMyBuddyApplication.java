54bf39fe7dd7a7784595c424f0c78ca1437ba820
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-07 12:04:42 +0100

    modification pour lire les controller

--- src/main/java/com/joselct17/PayMyBuddy/PayMyBuddyApplication.java
+++ src/main/java/com/joselct17/PayMyBuddy/PayMyBuddyApplication.java
@@ -1,23 +1,16 @@
 package com.joselct17.PayMyBuddy;
 
-import com.joselct17.PayMyBuddy.model.User;
-import com.joselct17.PayMyBuddy.repository.UserRepository;
-import com.joselct17.PayMyBuddy.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@ComponentScan("com.joselct17.PayMyBuddy")
-@EnableJpaRepositories("com.joselct17.PayMyBuddy.repository")
+@ComponentScan({"com.joselct17.PayMyBuddy"})
 public class PayMyBuddyApplication extends SpringBootServletInitializer {
 
-	@Autowired
-	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
