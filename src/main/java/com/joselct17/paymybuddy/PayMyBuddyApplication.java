f876eefef7755c39ecdb059533173b9e76d3b61b
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-08 16:55:20 +0100

    j'ai enlevé compoenentScan et EntityScan

--- src/main/java/com/joselct17/paymybuddy/PayMyBuddyApplication.java
+++ src/main/java/com/joselct17/paymybuddy/PayMyBuddyApplication.java
@@ -3,14 +3,11 @@ package com.joselct17.paymybuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan({"com.joselct17.paymybuddy.service"})
-@EntityScan({"com.joselct17.paymybuddy.model"})
 public class PayMyBuddyApplication extends SpringBootServletInitializer {
 
 
