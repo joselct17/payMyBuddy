6045eef72c46cc104a43bd7a7700d93ba0444035
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-08 16:54:59 +0100

    rajout du fichier

---
+++ src/test/java/com/joselct17/paymybuddy/repository/IUserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.joselct17.paymybuddy.repository;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IUserRepositoryTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+}
