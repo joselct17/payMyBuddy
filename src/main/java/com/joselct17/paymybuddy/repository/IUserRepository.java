779f4e2301223fa98a7147603e990cb4c898bf0d
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-09 10:42:34 +0100

    j'ai rajouté findByEmail

--- src/main/java/com/joselct17/paymybuddy/repository/IUserRepository.java
+++ src/main/java/com/joselct17/paymybuddy/repository/IUserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IUserRepository extends CrudRepository<User, Integer> {
 
+    public User findByEmail(String email);
+
 }
