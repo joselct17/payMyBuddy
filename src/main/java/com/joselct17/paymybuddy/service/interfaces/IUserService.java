8124376bdecd579a278fbaeb3312c3bf02c35351
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-09 10:42:48 +0100

    j'ai cree le fichier

---
+++ src/main/java/com/joselct17/paymybuddy/service/interfaces/IUserService.java
@@ -0,0 +1,19 @@
+package com.joselct17.paymybuddy.service.interfaces;
+
+import com.joselct17.paymybuddy.model.User;
+
+public interface IUserService {
+
+
+    void create(User user);
+
+
+    User findById(Integer id);
+
+
+    User findByEmail(String email);
+
+
+
+
+}
