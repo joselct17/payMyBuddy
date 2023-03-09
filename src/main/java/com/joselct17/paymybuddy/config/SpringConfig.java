d00ad40d71e786a77bdf9a5f2f006373f427f634
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-09 10:19:02 +0100

    creation du fichier mapper pour les DTO

---
+++ src/main/java/com/joselct17/paymybuddy/config/SpringConfig.java
@@ -0,0 +1,19 @@
+package com.joselct17.paymybuddy.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    //This is for mapping Entity-DTO
+    //https://www.baeldung.com/entity-to-and-from-dto-for-a-java-spring-application
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+
+}
