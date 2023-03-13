f5bb240596c6842301c62bc0ae45f7b709b8f09a
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-13 16:27:55 +0100

    rajout du test PostRegistrationForm_shouldFailcauseDifferentPasswords()

--- src/test/java/com/joselct17/paymybuddy/controller/RegistrationControllerTest.java
+++ src/test/java/com/joselct17/paymybuddy/controller/RegistrationControllerTest.java
@@ -1,17 +1,18 @@
 package com.joselct17.paymybuddy.controller;
 
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,11 +21,16 @@ public class RegistrationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ModelMapper modelMapperMock;
+
     @Test
     void getRegistration() throws Exception {
         mockMvc.perform(get("/registration"))
-                .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("registration"))
+                .andExpect(model().size(1))
+                .andExpect(model().attributeExists("userForm"));
     }
 
     @Test
@@ -39,6 +45,20 @@ public class RegistrationControllerTest {
                         .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    void PostRegistrationForm_shouldFailcauseDifferentPasswords() throws Exception {
+        mockMvc.perform(post("/registration")
+                        .param("firstName", "john")
+                        .param("lastName", "doe")
+                        .param("email", "johndoe@mail.com")
+                        .param("password", "123")
+                        .param("confirmPassword", "123456789")
+                        .param("bankAccount", "1AX123456789")
+                        .with(csrf()))
+                .andExpect(model().attributeErrorCount("userForm", 1)) //error to display in registration page
+                .andExpect(status().isOk()); //registration page reloaded
     }
 
 
