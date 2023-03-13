9784782feca4c876338401edc6e9956a2bf916a5
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-13 11:02:23 +0100

    findByroleName

--- src/main/java/com/joselct17/paymybuddy/repository/IRolesRepository.java
+++ src/main/java/com/joselct17/paymybuddy/repository/IRolesRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IRolesRepository extends CrudRepository<Role, Integer> {
 
+    Role findByroleName(String roleName);
 }
