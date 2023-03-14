1e6ef7da42303276c5c620a5793cdae9ebf669fa
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-14 10:14:55 +0100

    changement du nom du fichier

--- src/main/java/com/joselct17/paymybuddy/config/SecurityConfiguration.java
+++ src/main/java/com/joselct17/paymybuddy/config/SecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Configuration
-public class SpringSecurityConfig {
+public class SecurityConfiguration {
 
     /**
      * Spring Security needs to have a PasswordEncoder defined.
