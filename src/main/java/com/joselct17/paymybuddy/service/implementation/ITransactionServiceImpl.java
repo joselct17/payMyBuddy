874b40762c1af518595ed9e83555621afbefffaa
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-15 10:35:02 +0100

    creation d el'interface ITransactionServiceImpl

---
+++ src/main/java/com/joselct17/paymybuddy/service/implementation/ITransactionServiceImpl.java
@@ -0,0 +1,12 @@
+package com.joselct17.paymybuddy.service.implementation;
+
+import com.joselct17.paymybuddy.service.interfaces.ITransactionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ITransactionServiceImpl implements ITransactionService {
+    @Override
+    public Object getCurrentUserUserTransactionPage(int pageNumber, int size) {
+        return null;
+    }
+}
