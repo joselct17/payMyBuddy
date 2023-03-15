2775533b424367ab9bcdb433676c71374c3019de
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-15 14:37:27 +0100

    implementation ConnectionController

---
+++ src/main/java/com/joselct17/paymybuddy/controller/ConnectionController.java
@@ -0,0 +1,25 @@
+package com.joselct17.paymybuddy.controller;
+
+import com.joselct17.paymybuddy.service.interfaces.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConnectionController {
+
+    @Autowired
+    IUserService iUserService;
+
+
+    @GetMapping("/connection")
+    public String connection(@RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber,
+                             @RequestParam(value = "size", required = false, defaultValue = "5") int size,
+                             Model model) {
+        model.addAttribute("paged", iUserService.getCurrentUserConnectionPage(pageNumber, size));
+        return "connection";
+    }
+
+}
