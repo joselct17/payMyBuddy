52489ace7b7c0506621e42d4c8186e4cf2d3132d
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 09:07:05 +0100

    rajout de du fichier de test ppour le controller connection

---
+++ src/test/java/com/joselct17/paymybuddy/controller/ConnectionControllerTest.java
@@ -0,0 +1,73 @@
+package com.joselct17.paymybuddy.controller;
+
+
+import com.joselct17.paymybuddy.model.User;
+import com.joselct17.paymybuddy.service.interfaces.IUserService;
+import com.joselct17.paymybuddy.utils.paging.Paged;
+import com.joselct17.paymybuddy.utils.paging.Paging;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import java.util.Arrays;
+import java.util.Currency;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ConnectionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IUserService userService;
+
+    User user1;
+    User user2;
+    User user3;
+    Paged<User> paged;
+
+    @BeforeEach
+    void setup() {
+        user1 = new User(1, "firstname1", "lastname1", "user1e@mail.com","password1", "1AX256", Currency.getInstance("USD"),  new HashSet<>(), new HashSet<>(), new HashSet<>() );
+        user2 = new User(2, "firstname2", "lastname2", "user2e@mail.com", "password2",  "1AX256", Currency.getInstance("USD"), new HashSet<>(), new HashSet<>(), new HashSet<>() );
+        user3 = new User(3, "firstname3", "lastname3", "user3e@mail.com","password3", "1AX256", Currency.getInstance("USD"),  new HashSet<>(), new HashSet<>(), new HashSet<>() );
+
+        User[] userArray = {user1,user2,user3};
+        List<User> users = Arrays.asList(userArray);
+        Page<User> pagedUser = new PageImpl<User>(users);
+
+        Paging paging = Paging.of(1, 1);//, 5);
+        paged = new Paged<User>(pagedUser, paging);
+
+    }
+
+    @WithUserDetails("user@company.com") //user from SpringSecurityWebTestConfig.class
+    @Test
+    void GetConnectionPage_shouldSucceed() throws Exception {
+        //ARRANGE:
+        when(userService.getCurrentUserConnectionPage(1, 5)).thenReturn(paged); //display list of connections
+
+        //ACT+ASSERT:
+        mockMvc.perform(get("/connection"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("connection"))
+                .andExpect(model().size(1))
+                .andExpect(model().attributeExists("paged"));
+    }
+
+}
