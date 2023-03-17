4702e4f789cea33469d639e5b07dfb23d2345275
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 09:07:48 +0100

    getCurrentUserConnectionPage

--- src/main/java/com/joselct17/paymybuddy/service/interfaces/IUserService.java
+++ src/main/java/com/joselct17/paymybuddy/service/interfaces/IUserService.java
@@ -1,6 +1,7 @@
 package com.joselct17.paymybuddy.service.interfaces;
 
 import com.joselct17.paymybuddy.model.User;
+import com.joselct17.paymybuddy.utils.paging.Paged;
 
 public interface IUserService {
 
@@ -17,4 +18,6 @@ public interface IUserService {
     Boolean existsByEmail(String email);
 
     User getCurrentUser();
+
+    Paged<User> getCurrentUserConnectionPage(int pageNumber, int size);
 }
