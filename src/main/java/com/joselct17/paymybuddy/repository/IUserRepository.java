40d34e8468343971e60c21e5255321c3e3cb12af
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 09:07:41 +0100

    findConnectionById

--- src/main/java/com/joselct17/paymybuddy/repository/IUserRepository.java
+++ src/main/java/com/joselct17/paymybuddy/repository/IUserRepository.java
@@ -1,7 +1,11 @@
 package com.joselct17.paymybuddy.repository;
 
 import com.joselct17.paymybuddy.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +14,7 @@ public interface IUserRepository extends CrudRepository<User, Integer> {
     public User findByEmail(String email);
 
     public Boolean existsByEmail(String email);
+
+
+    public Page<User> findConnectionById(@Param("id") Integer id, Pageable pageRequest);
 }
