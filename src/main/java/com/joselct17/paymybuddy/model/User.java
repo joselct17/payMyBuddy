bd58632032e51bb4a2467b0868c851b856db9e5f
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 09:00:44 +0100

    rajout des getter et setter avec lombok

--- src/main/java/com/joselct17/paymybuddy/model/User.java
+++ src/main/java/com/joselct17/paymybuddy/model/User.java
@@ -4,7 +4,9 @@ package com.joselct17.paymybuddy.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.Currency;
@@ -14,6 +16,8 @@ import java.util.Set;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "user")
 public class User {
 
@@ -43,7 +47,7 @@ public class User {
     orphanRemoval = true,
     fetch = FetchType.EAGER)
     @JoinColumn(name="user_id")
-    List<BankTransaction> bankTransactions = new ArrayList<>();
+    Set<BankTransaction> bankTransactions;
 
 
     @OneToMany(
@@ -51,7 +55,7 @@ public class User {
     orphanRemoval = true,
     fetch = FetchType.EAGER)
     @JoinColumn(name="user_id")
-    List<Transaction> transactions = new ArrayList<>();
+    Set<Transaction> transactions;
 
 
     @ManyToMany
@@ -62,87 +66,4 @@ public class User {
     private Set<Role> roles;
 
 
-
-
-    //GETTER-SETTER
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getBankAccount() {
-        return bankAccount;
-    }
-
-    public void setBankAccount(String bankAccount) {
-        this.bankAccount = bankAccount;
-    }
-
-    public List<BankTransaction> getBankTransactions() {
-        return bankTransactions;
-    }
-
-    public void setBankTransactions(List<BankTransaction> bankTransactions) {
-        this.bankTransactions = bankTransactions;
-    }
-
-    public List<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    public void setTransactions(List<Transaction> transactions) {
-        this.transactions = transactions;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public Currency getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(Currency currency) {
-        this.currency = currency;
-    }
 }
