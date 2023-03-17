c9556f18a3f849a2cd51a926e8b75eebe30b7d82
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 09:00:18 +0100

    rajout des getter et setter avec lombok

--- src/main/java/com/joselct17/paymybuddy/model/BankTransaction.java
+++ src/main/java/com/joselct17/paymybuddy/model/BankTransaction.java
@@ -4,12 +4,16 @@ package com.joselct17.paymybuddy.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "banktransaction")
 public class BankTransaction {
 
@@ -25,37 +29,4 @@ public class BankTransaction {
     @Column(name = "currency")
     private String currency;
 
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
 }
