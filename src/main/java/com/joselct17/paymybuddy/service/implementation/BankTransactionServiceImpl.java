1dc042da72dd594af9b81469a76a47a208b91e11
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 14:56:05 +0100

    creation du fichier

---
+++ src/main/java/com/joselct17/paymybuddy/service/implementation/BankTransactionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.joselct17.paymybuddy.service.implementation;
+
+import com.joselct17.paymybuddy.model.BankTransaction;
+import com.joselct17.paymybuddy.service.interfaces.IBankTransactionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankTransactionServiceImpl implements IBankTransactionService {
+    @Override
+    public void create(BankTransaction bankTransactionToCreate) {
+
+    }
+}
