1542f2963e5d1ad573adabe0a347494f08278511
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 09:07:15 +0100

    rajout de create

--- src/main/java/com/joselct17/paymybuddy/service/interfaces/ITransactionService.java
+++ src/main/java/com/joselct17/paymybuddy/service/interfaces/ITransactionService.java
@@ -1,5 +1,12 @@
 package com.joselct17.paymybuddy.service.interfaces;
 
+import com.joselct17.paymybuddy.model.Transaction;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
 public interface ITransactionService {
     Object getCurrentUserUserTransactionPage(int pageNumber, int size);
+
+    void create(Transaction userTransactionToCreate, Map<String, BigDecimal> fees);
 }
