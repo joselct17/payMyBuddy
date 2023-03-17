a80b3789adf03623dd4fb6c2058e3053d39c631c
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 09:07:31 +0100

    impl de la focntion create

--- src/main/java/com/joselct17/paymybuddy/service/implementation/ITransactionServiceImpl.java
+++ src/main/java/com/joselct17/paymybuddy/service/implementation/ITransactionServiceImpl.java
@@ -1,12 +1,46 @@
 package com.joselct17.paymybuddy.service.implementation;
 
+import com.joselct17.paymybuddy.model.Transaction;
+import com.joselct17.paymybuddy.model.User;
+import com.joselct17.paymybuddy.repository.ITransactionRepository;
+import com.joselct17.paymybuddy.service.interfaces.ILocalDateTimeService;
 import com.joselct17.paymybuddy.service.interfaces.ITransactionService;
+import com.joselct17.paymybuddy.service.interfaces.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @Service
 public class ITransactionServiceImpl implements ITransactionService {
+
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private ILocalDateTimeService localDateTimeService;
+
+    @Autowired
+    private ITransactionRepository transactionRepository;
+
     @Override
     public Object getCurrentUserUserTransactionPage(int pageNumber, int size) {
         return null;
+    }
+
+    @Override
+    public void create(Transaction transaction, Map<String, BigDecimal> fees) {
+
+        User currentUser = userService.getCurrentUser();
+
+        transaction.setDateTime(localDateTimeService.now());
+        transaction.setUserSource(currentUser);
+        transaction.setFees(fees.get("fees"));
+        transaction.setAmount(fees.get("finalAmount"));
+
+        transactionRepository.save(transaction);
+
+
     }
 }
