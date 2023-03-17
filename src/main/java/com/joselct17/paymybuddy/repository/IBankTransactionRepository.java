71162f9e7310eeebd61c63fd95a60c904e1ccd7c
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 14:55:13 +0100

    changement du nom de la classe

--- src/main/java/com/joselct17/paymybuddy/repository/IBankTransactionRepository.java
+++ src/main/java/com/joselct17/paymybuddy/repository/IBankTransactionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IBankTransaction extends CrudRepository<BankTransaction, Integer> {
+public interface IBankTransactionRepository extends CrudRepository<BankTransaction, Integer> {
 }
