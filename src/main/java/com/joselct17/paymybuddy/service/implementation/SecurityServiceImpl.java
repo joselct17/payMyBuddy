d5ab5b256bb6e663a884feeac967fe722c88e98a
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-17 09:03:24 +0100

    rajout de la fonction getCurrentUserDetailsUserName

--- src/main/java/com/joselct17/paymybuddy/service/implementation/SecurityServiceImpl.java
+++ src/main/java/com/joselct17/paymybuddy/service/implementation/SecurityServiceImpl.java
@@ -1,6 +1,12 @@
 package com.joselct17.paymybuddy.service.implementation;
 
+
 import com.joselct17.paymybuddy.service.interfaces.ISecurityService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +18,18 @@ public class SecurityServiceImpl implements ISecurityService {
 
     @Override
     public void autoLogin(String username, String password) {
+    }
+
+    @Override
+    public String getCurrentUserDetailsUserName() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            Object principal = auth.getPrincipal();
+            if (principal instanceof User) {
+                return ((User) principal).getUsername();
+            }
+        }
+        return null;
+
     }
 }
