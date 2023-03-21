1d59820171718a99307605109bc47a12fc619680
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-21 17:23:13 +0100

    rajout de findAll()

--- src/main/java/com/joselct17/paymybuddy/service/interfaces/IUserService.java
+++ src/main/java/com/joselct17/paymybuddy/service/interfaces/IUserService.java
@@ -3,8 +3,14 @@ package com.joselct17.paymybuddy.service.interfaces;
 import com.joselct17.paymybuddy.model.User;
 import com.joselct17.paymybuddy.utils.paging.Paged;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+
 public interface IUserService {
 
+
+    Iterable<User> findAll();
 
     void create(User user);
 
@@ -20,4 +26,6 @@ public interface IUserService {
     User getCurrentUser();
 
     Paged<User> getCurrentUserConnectionPage(int pageNumber, int size);
+
+    BigDecimal sumAmountCalculate(User userSource, BigDecimal negate, Currency currency);
 }
