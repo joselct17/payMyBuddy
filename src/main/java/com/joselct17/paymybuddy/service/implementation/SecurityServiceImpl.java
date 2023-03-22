dae70a21a22f4473ac66c3914167016022a361ae
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-22 14:08:32 +0100

    implement autoLogin et is Autenticated

--- src/main/java/com/joselct17/paymybuddy/service/implementation/SecurityServiceImpl.java
+++ src/main/java/com/joselct17/paymybuddy/service/implementation/SecurityServiceImpl.java
@@ -1,7 +1,11 @@
 package com.joselct17.paymybuddy.service.implementation;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.joselct17.paymybuddy.service.interfaces.ISecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -11,13 +15,37 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityServiceImpl implements ISecurityService {
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
     @Override
     public boolean isAuthenticated() {
-        return false;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null ) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 
     @Override
     public void autoLogin(String username, String password) {
+        logger.debug("SecurityServiceImpl.autoLogin(Username={},password={})",username,password);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+
+        authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        if (usernamePasswordAuthenticationToken.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            logger.debug("Auto login {} successfully!", username);
+        }
     }
 
     @Override
