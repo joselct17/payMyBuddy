641f70b43aa8e6efc67edebe53f8e80daf54d05a
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-31 17:25:41 +0200

    rajout de la fonctionnalité findUserTransactionByUserId avec un query costum pour acceder aux transactions en dependance du id

--- src/main/java/com/joselct17/paymybuddy/repository/ITransactionRepository.java
+++ src/main/java/com/joselct17/paymybuddy/repository/ITransactionRepository.java
@@ -1,9 +1,22 @@
 package com.joselct17.paymybuddy.repository;
 
 import com.joselct17.paymybuddy.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ITransactionRepository extends CrudRepository<Transaction, Integer> {
+
+    @Query(value =
+            "SELECT * "
+                    + "FROM transaction "
+                    + "WHERE usersource_id = :usersourceid OR userdestination_id = :usersourceid "
+            ,
+            nativeQuery = true)
+    Page<Transaction> findUserTransactionByUserId(@Param("usersourceid") int usersourceid, Pageable pageRequest);
 }
