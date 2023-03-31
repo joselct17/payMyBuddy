5c8a3c953c8eb031da5b6be0d808d6e0cca41345
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-31 17:26:01 +0200

    changement du type d'objet

--- src/main/java/com/joselct17/paymybuddy/service/interfaces/ITransactionService.java
+++ src/main/java/com/joselct17/paymybuddy/service/interfaces/ITransactionService.java
@@ -1,12 +1,13 @@
 package com.joselct17.paymybuddy.service.interfaces;
 
 import com.joselct17.paymybuddy.model.Transaction;
+import com.joselct17.paymybuddy.utils.paging.Paged;
 
 import java.math.BigDecimal;
 import java.util.Map;
 
 public interface ITransactionService {
-    Object getCurrentUserUserTransactionPage(int pageNumber, int size);
+    Paged<Transaction> getCurrentUserUserTransactionPage(int pageNumber, int size);
 
     void create(Transaction userTransactionToCreate, Map<String, BigDecimal> fees);
 }
