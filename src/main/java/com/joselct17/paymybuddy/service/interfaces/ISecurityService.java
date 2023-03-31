28a6bcb11dc9f7dfe82e9d31294ddcbb4b4b3885
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-31 17:24:52 +0200

    commentaire autoLogin

--- src/main/java/com/joselct17/paymybuddy/service/interfaces/ISecurityService.java
+++ src/main/java/com/joselct17/paymybuddy/service/interfaces/ISecurityService.java
@@ -1,11 +1,12 @@
 package com.joselct17.paymybuddy.service.interfaces;
 
+import org.springframework.security.core.Authentication;
+
 public interface ISecurityService {
 
     boolean isAuthenticated();
 
-    void autoLogin(String username, String password);
-
+   // void autoLogin(String username, String password);
 
     public String getCurrentUserDetailsUserName();
 }
