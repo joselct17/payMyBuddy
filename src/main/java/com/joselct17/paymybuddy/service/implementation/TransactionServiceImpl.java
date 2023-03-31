4e62f38ebdc0584219ad9ac76814affa8a7d97a2
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-03-31 17:35:05 +0200

    implementation de la donction qui doit prendre les utilisateurs connectées et envoyer ses transactions

--- src/main/java/com/joselct17/paymybuddy/service/implementation/TransactionServiceImpl.java
+++ src/main/java/com/joselct17/paymybuddy/service/implementation/TransactionServiceImpl.java
@@ -1,12 +1,18 @@
 package com.joselct17.paymybuddy.service.implementation;
 
+import com.joselct17.paymybuddy.controller.TransactionController;
 import com.joselct17.paymybuddy.model.Transaction;
 import com.joselct17.paymybuddy.model.User;
 import com.joselct17.paymybuddy.repository.ITransactionRepository;
 import com.joselct17.paymybuddy.service.interfaces.ILocalDateTimeService;
+import com.joselct17.paymybuddy.service.interfaces.IPagingService;
 import com.joselct17.paymybuddy.service.interfaces.ITransactionService;
 import com.joselct17.paymybuddy.service.interfaces.IUserService;
+import com.joselct17.paymybuddy.utils.paging.Paged;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -24,9 +30,16 @@ public class TransactionServiceImpl implements ITransactionService {
     @Autowired
     private ITransactionRepository transactionRepository;
 
+    @Autowired
+    private IPagingService pagingService;
+
     @Override
-    public Object getCurrentUserUserTransactionPage(int pageNumber, int size) {
-        return null;
+    public Paged<Transaction> getCurrentUserUserTransactionPage(int pageNumber, int size) {
+
+        PageRequest request = PageRequest.of(pageNumber - 1, size, Sort.by(Sort.Direction.DESC, "id"));
+        Page<Transaction> page = transactionRepository.findUserTransactionByUserId(userService.getCurrentUser().getId(),request);
+
+        return new Paged<>(page, pagingService.of(page.getTotalPages(), pageNumber));
     }
 
     @Override
