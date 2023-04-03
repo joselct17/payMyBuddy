90870692bc9961982081a620d292575370d504b6
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-03 15:05:29 +0200

    rajout de la requette query findBankTransactionByUserId

--- src/main/java/com/joselct17/paymybuddy/repository/IBankTransactionRepository.java
+++ src/main/java/com/joselct17/paymybuddy/repository/IBankTransactionRepository.java
@@ -1,9 +1,20 @@
 package com.joselct17.paymybuddy.repository;
 
 import com.joselct17.paymybuddy.model.BankTransaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IBankTransactionRepository extends CrudRepository<BankTransaction, Integer> {
+
+    @Query(value =
+            "SELECT * "
+                    + "FROM banktransaction t "
+                    + "WHERE t.user_id = :userid",
+            nativeQuery = true)
+    public Page<BankTransaction> findBankTransactionByUserId(@Param("userid") int userid, Pageable pageRequest);
 }
