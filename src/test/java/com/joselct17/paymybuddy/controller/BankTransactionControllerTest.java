b8858d3a9a98923c8ddd672057f34f66ee77ef50
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-17 17:13:59 +0200

    rajout de before each

--- src/test/java/com/joselct17/paymybuddy/controller/BankTransactionControllerTest.java
+++ src/test/java/com/joselct17/paymybuddy/controller/BankTransactionControllerTest.java
@@ -3,12 +3,17 @@ package com.joselct17.paymybuddy.controller;
 
 import com.joselct17.paymybuddy.model.BankTransaction;
 import com.joselct17.paymybuddy.model.User;
+import com.joselct17.paymybuddy.repository.IUserRepository;
 import com.joselct17.paymybuddy.service.interfaces.IUserService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -37,43 +42,28 @@ public class BankTransactionControllerTest {
     private MockMvc mockMvc;
 
 
+    @BeforeEach
+    void clearDatabase(@Autowired JdbcTemplate jdbcTemplate) {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "banktransaction");
+        jdbcTemplate.update("DELETE FROM user_roles");
+        jdbcTemplate.update("DELETE FROM user");
+        jdbcTemplate.update("INSERT INTO user (first_name, last_name, amount, currency, bank_account, date_time_inscription, email, password) " +
+                "VALUES ('John', 'Doe', '1000', 'USD', 'XACSFDC', '2023-04-07 09:23:11.673329', 'john@doe.com', 'VDGZBZJBdegbh541'), " +
+                "('Jane', 'Doe', '2000', 'USD', 'XACSFDC', '2023-04-07 09:23:11.673329', 'jane@doe.com', 'VDGZBZJBdegbh542')");    }
+
+
+
     @Test
-    @WithMockUser(username = "test@test.com")
+    @WithMockUser(username = "john@doe.com")
     void getBankTransaction() throws Exception {
         mockMvc.perform(get("/banktransaction"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
-    @Test
-    @WithMockUser(username="test@test.com") //test@mail.com exists in our test database
-    void postBanktransactionSendMoney() throws Exception {
-        mockMvc.perform(post("/banktransaction")
-                        .param("getOrSendRadioOptions", "send")
-                        .param("amount", "100")
-                        .param("currency", "USD")
-                        .with(csrf())
-                )//.andDo(print())
-                .andExpect(status().is3xxRedirection());
-
-        User userTest = userService.findByEmail("test@test.com");
-        assertEquals(new BigDecimal("800.00"),userTest.getAmount(), "1000 - bank transaction");
-        Set<BankTransaction> banktransactions = userTest.getBankTransactions();
-        assertEquals(1,banktransactions.size());
-
-        BankTransaction bankTransaction = banktransactions.iterator().next();
-        assertEquals(userTest, bankTransaction.getUser());
-        //get time in second between transaction datetime and now :
-        long durationInSec = Duration.between(bankTransaction.getDateTime(), LocalDateTime.now()).getSeconds();
-        assertTrue(durationInSec<2, "time difference between transaction time and now can't be more than 2s");
-        assertEquals(new BigDecimal("-100"),bankTransaction.getAmount());
-        assertEquals(Currency.getInstance("USD"),bankTransaction.getCurrency());
-
-
-    }
 
     @Test
-    @WithMockUser(username="test@test.com") //test@mail.com exists in our test database
+    @WithMockUser(username="john@doe.com") //test@test.com exists in our test database
     void postBanktransactionGetMoney() throws Exception {
         mockMvc.perform(post("/banktransaction")
                         .param("getOrSendRadioOptions", "get")
@@ -83,17 +73,43 @@ public class BankTransactionControllerTest {
                 )//.andDo(print())
                 .andExpect(status().is3xxRedirection());
 
-        User userTest = userService.findByEmail("test@test.com");
-        assertEquals(new BigDecimal("1100.00"),userTest.getAmount(), "1000 + bank transaction");
+        User userTest = userService.findByEmail("john@doe.com");
+        assertEquals(new BigDecimal("1100.00"), userTest.getAmount(), "1000 + bank transaction");
+        Set<BankTransaction> banktransactions = userTest.getBankTransactions();
+        assertEquals(1, banktransactions.size());
+
+        BankTransaction bankTransaction = banktransactions.iterator().next();
+        assertEquals(userTest, bankTransaction.getUser());
+        //get time in second between transaction datetime and now :
+        long durationInSec = Duration.between(bankTransaction.getDateTime(), LocalDateTime.now()).getSeconds();
+        assertTrue(durationInSec < 2, "time difference between transaction time and now can't be more than 2s");
+        assertEquals(new BigDecimal("100.00"), bankTransaction.getAmount());
+        assertEquals(Currency.getInstance("USD"), bankTransaction.getCurrency());
+
+    }
+
+    @Test // envoi d'argent vers le compte en banque
+    @WithMockUser(username="john@doe.com") //test@test.com exists in our test database
+    void postBanktransactionSendMoney() throws Exception {
+        mockMvc.perform(post("/banktransaction")
+                        .param("getOrSendRadioOptions", "send")
+                        .param("amount", "100")
+                        .param("currency", "USD")
+                        .with(csrf())
+                )//.andDo(print())
+                .andExpect(status().is3xxRedirection());
+
+        User userTest = userService.findByEmail("john@doe.com");
+        assertEquals(new BigDecimal("900.00"),userTest.getAmount(), "100 - bank transaction");
         Set<BankTransaction> banktransactions = userTest.getBankTransactions();
         assertEquals(1,banktransactions.size());
 
         BankTransaction bankTransaction = banktransactions.iterator().next();
         assertEquals(userTest, bankTransaction.getUser());
         //get time in second between transaction datetime and now :
-        long durationInSec = Duration.between(bankTransaction.getDateTime(),LocalDateTime.now()).getSeconds();
+        long durationInSec = Duration.between(bankTransaction.getDateTime(), LocalDateTime.now()).getSeconds();
         assertTrue(durationInSec<2, "time difference between transaction time and now can't be more than 2s");
-        assertEquals(new BigDecimal("100"),bankTransaction.getAmount());
+        assertEquals(new BigDecimal("-100.00"),bankTransaction.getAmount());
         assertEquals(Currency.getInstance("USD"),bankTransaction.getCurrency());
 
     }
