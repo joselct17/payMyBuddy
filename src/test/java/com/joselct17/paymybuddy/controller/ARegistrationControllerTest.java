727f8de8a60520faa502d4ab6a2826d709d30243
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-04-17 17:13:12 +0200

    rajout de PostRegistrationForm_shouldFailcauseUserAlreadyExist()

--- src/test/java/com/joselct17/paymybuddy/controller/ARegistrationControllerTest.java
+++ src/test/java/com/joselct17/paymybuddy/controller/ARegistrationControllerTest.java
@@ -1,25 +1,24 @@
 package com.joselct17.paymybuddy.controller;
 
-import com.joselct17.paymybuddy.repository.IUserRepository;
+import com.joselct17.paymybuddy.service.interfaces.IUserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 
-import java.math.BigDecimal;
-
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -29,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class RegistrationControllerTest {
+public class ARegistrationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -39,8 +38,11 @@ public class RegistrationControllerTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
-    
+
+    @MockBean
+    IUserService userService;
+
+
     @BeforeEach
     void clearDatabase(@Autowired JdbcTemplate jdbcTemplate) {
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "user_roles","user" );
@@ -84,9 +86,28 @@ public class RegistrationControllerTest {
                         .param("amount", "200")
                         .param("currency", "USD")
                         .with(csrf()))
-                //.andExpect(model().attributeErrorCount("userForm", 1)) //error to display in registration page
+                .andExpect(status().is3xxRedirection()); //registration page reloaded
+    }
+
+    @Test
+    void PostRegistrationForm_shouldFailcauseUserAlreadyExist() throws Exception {
+        //ARRANGE
+        when(userService.existsByEmail("BingBong@mail.com")).thenReturn(Boolean.TRUE);
+
+        //ACT+ASSERT
+        mockMvc.perform(post("/registration")
+                        .param("firstname", "Bing")
+                        .param("lastname", "Bong")
+                        .param("email", "BingBong@mail.com")
+                        .param("password", "123")
+                        .param("confirmPassword", "123")
+                        .param("bankaccountnumber", "1AX123456789")
+                        .param("currency", "USD")
+                        .with(csrf()))
+                .andExpect(model().attributeErrorCount("userForm", 1)) //error to display in registration page
                 .andExpect(status().isOk()); //registration page reloaded
     }
+
 
 
 }
